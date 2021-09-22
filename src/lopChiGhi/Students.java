133d1239a21fe2c67b0eb75773eec9958ef37713
cuongtien00 <cuonghiep000@gmail.com>
2021-09-22 11:33:07 +0700

    add LopChiGhi

---
+++ src/lopChiGhi/Students.java
@@ -0,0 +1,17 @@
+package lopChiGhi;
+
+public class Students {
+    private String name = "John";
+    private String classes = "C02";
+
+    public Students() {
+    }
+
+     void setName(String name) {
+        this.name = name;
+    }
+
+    void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ src/lopChiGhi/TestAccessModifier.java
@@ -0,0 +1,10 @@
+package lopChiGhi;
+
+public class TestAccessModifier {
+    public static void main(String[] args) {
+        Students s1 = new Students();
+
+       s1.setName("Cuong tien");
+       s1.setName("C08H1");
+    }
+}
